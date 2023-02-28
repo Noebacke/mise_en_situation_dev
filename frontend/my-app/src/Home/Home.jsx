57ef5a081d32a3917254bbc2dedc24ad307f2682
Etienne Decourselle <decourselle.etienne@hotmail.fr>
2023-02-28 09:16:57 +0100

    Home page

--- frontend/my-app/src/Home/Home.jsx
+++ frontend/my-app/src/Home/Home.jsx
@@ -1,9 +1,17 @@
 import './Home.scss';
+import fleche_gauche from '../img/fleche_gauche.svg'
 
 const Home = () => {
     return (
         <>
-
+            <div className='bloc-lien'>
+                <a href='horaires'>Horaires en temps réel</a>
+                <img src={fleche_gauche} alt='vers les horaires' />
+            </div>
+            <div className='bloc-lien'>
+                <a href='trajet'>Calculer son trajet</a>
+                <img src={fleche_gauche} alt='vers le calcul du trajet' />
+            </div>
         </>
     )
 }
