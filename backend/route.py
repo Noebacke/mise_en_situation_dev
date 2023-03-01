d4804e5a34ff4cbf92df5d659a30326ab773c549
Etienne Decourselle <decourselle.etienne@hotmail.fr>
2023-03-01 13:26:56 +0100

    script v1

--- backend/route.py
+++ backend/route.py
@@ -6,8 +6,8 @@ callApiSwappi = "https://swapi.dev/api/planets/1/"
 
 app = FastAPI()
 
+
 @app.post("/scheduletrame")
 def post_params(test: str, filter: int):
     response = requests.get(callApi)
     return response.json()["history"][2]["payload"]["zip_metadata"]
-   
