1cb7b8297e33a7aadeaafc2c99cc24bb50bd9cc3
Noé Backé <noeebacke@gmail.com>
2023-03-01 15:22:08 +0100

    push of back

--- backend/extractXml.py
+++ backend/extractXml.py
@@ -1,7 +1,23 @@
 import xmltodict
 import json
+import re 
 
 with open('./data/BORDEAUX_METROPOLE_offre_Bus_TBM_BordeauxM_tropole__95_95.xml') as file:
+
+    dbData = []
+
     dictionary = xmltodict.parse(file.read())
-json_object = json.dumps(dictionary)
-print(json_object)
+
+    pattern = dictionary["PublicationDelivery"]["dataObjects"]["CompositeFrame"][
+        "frames"]["GeneralFrame"][0]["members"]
+
+    json_object = json.dumps(pattern["ScheduledStopPoint"][0]["@id"],indent=3)
+    regExp = re.compile('[A-Za-z:_]+95_[A-Z0-9]+a([0-9]+)')
+
+    for match in regExp.finditer(json_object):
+        match.group(1)
+        dbData.append(match.group(1))
+    print(dbData)
+    
+    
+    
