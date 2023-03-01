0e5ae98dc425afec93ef0435f0b0cf77d7d97a03
Etienne Decourselle <decourselle.etienne@hotmail.fr>
2023-03-01 15:21:03 +0100

    liste d'ID fonctionnelle

--- backend/extractXml.py
+++ backend/extractXml.py
@@ -1,7 +1,33 @@
 import xmltodict
 import json
+import re
 
 with open('./data/BORDEAUX_METROPOLE_offre_Bus_TBM_BordeauxM_tropole__95_95.xml') as file:
     dictionary = xmltodict.parse(file.read())
+
 json_object = json.dumps(dictionary)
-print(json_object)
+
+stopPoints = dictionary['PublicationDelivery']['dataObjects']['CompositeFrame'][
+    'frames']['GeneralFrame'][0]['members']['ScheduledStopPoint']
+
+# print(stopPoints)
+
+stopPointsArray = []
+
+i = 0
+for stop in stopPoints:
+    stopPointsArray.append(stopPoints[i]['@id'])
+    i += 1
+# print(stopPointsArray)
+
+regexId = re.compile('[A-Za-z:_]+95_[A-Z0-9]+a([0-9]+)')
+
+i2 = 0
+listId = []
+
+for stop in stopPointsArray:
+    for match in regexId.finditer(stopPointsArray[i2]):
+        listId.append(match.group(1))
+        i2 += 1
+
+print(listId)
