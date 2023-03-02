d3c1ef6d02ba6a408743e7d07109ba862b1bbf87
Etienne Decourselle <decourselle.etienne@hotmail.fr>
2023-03-02 14:57:15 +0100

    test to root

--- backend/pymysql/pymysql_get_databases.py
+++ backend/pymysql/pymysql_get_databases.py
@@ -1,61 +1,66 @@
 import mysql.connector
 import json
 
+
 def deleteTable():
-  mydb = mysql.connector.connect(
-    host="localhost",
-    user="test",
-    password="123456",
-    database="ligne95"
-  )
-  cursor = mydb.cursor()
-  cursor.execute("DROP TABLE IF EXISTS STOPS")
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="123456",
+        database="ligne95"
+    )
+    cursor = mydb.cursor()
+    cursor.execute("DROP TABLE IF EXISTS STOPS")
 
-def createTable(): 
-  mydb = mysql.connector.connect(
-    host="localhost",
-    user="test",
-    password="123456",
-    database="ligne95"
-  )
-  cursor = mydb.cursor()
-  cursor.execute(
-    "CREATE TABLE STOPS (id INT AUTO_INCREMENT primary key NOT NULL, id_stop INT, name VARCHAR(100) NOT NULL, latitude FLOAT,longitude FLOAT, destination VARCHAR(100),\
+
+def createTable():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="123456",
+        database="ligne95"
+    )
+    cursor = mydb.cursor()
+    cursor.execute(
+        "CREATE TABLE STOPS (id INT AUTO_INCREMENT primary key NOT NULL, id_stop INT, name VARCHAR(100) NOT NULL, latitude FLOAT,longitude FLOAT, destination VARCHAR(100),\
         type VARCHAR(100), arrival VARCHAR(10), departure VARCHAR(10));"
-  )
+    )
 
-sql ="INSERT INTO STOPS (id_stop, name, latitude, longitude, destination, type, arrival, departure) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+
+sql = "INSERT INTO STOPS (id_stop, name, latitude, longitude, destination, type, arrival, departure) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+
 
 def insertData(e):
-  mydb = mysql.connector.connect(
-    host="localhost",
-    user="test",
-    password="123456",
-    database="ligne95"
-  )
-  cursor = mydb.cursor()
-  val=(e['id'], e['name'], e['latitude'], e['longitude'], e['destination'], e['type'], e['arrival'], e['departure'])
-  cursor.execute(sql,val)
-  mydb.commit()
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="123456",
+        database="ligne95"
+    )
+    cursor = mydb.cursor()
+    val = (e['id'], e['name'], e['latitude'], e['longitude'],
+           e['destination'], e['type'], e['arrival'], e['departure'])
+    cursor.execute(sql, val)
+    mydb.commit()
+
 
 def getDataFromDatabase():
-  mydb = mysql.connector.connect(
-    host="localhost",
-    user="test",
-    password="123456",
-    database="ligne95"
-  )
-  mycursor = mydb.cursor()
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="123456",
+        database="ligne95"
+    )
+    mycursor = mydb.cursor()
 
-  mycursor.execute("SELECT * FROM STOPS")
+    mycursor.execute("SELECT * FROM STOPS")
 
-  myresult = mycursor.fetchall()
-  print(myresult)
-  data = []
+    myresult = mycursor.fetchall()
+    print(myresult)
+    data = []
 
-  for row in myresult:
-    data.append({"id":row[0],"id_stop":row[1],"name":row[2], "latitude":row[3], "longitude":row[4], "destination":row[5], "type":row[6], "arrival":row[7], "departure":row[8]})
+    for row in myresult:
+        data.append({"id": row[0], "id_stop": row[1], "name": row[2], "latitude": row[3], "longitude": row[4],
+                    "destination": row[5], "type": row[6], "arrival": row[7], "departure": row[8]})
 
-  return data
-
-
+    return data
