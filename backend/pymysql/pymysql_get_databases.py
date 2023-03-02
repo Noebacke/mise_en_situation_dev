cac008973fc1ab5f69c7e13be303f9d4b6bb8179
Noé Backé <noeebacke@gmail.com>
2023-03-02 16:05:14 +0100

    get time of travel

--- backend/pymysql/pymysql_get_databases.py
+++ backend/pymysql/pymysql_get_databases.py
@@ -3,7 +3,6 @@ import json
 
 
 def deleteTable():
-<<<<<<< HEAD
   mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -23,28 +22,6 @@ def createTable():
   cursor = mydb.cursor()
   cursor.execute(
     "CREATE TABLE STOPS (id INT AUTO_INCREMENT primary key NOT NULL, id_stop INT, name VARCHAR(100) NOT NULL, latitude FLOAT,longitude FLOAT, destination VARCHAR(100),\
-=======
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="123456",
-        database="ligne95"
-    )
-    cursor = mydb.cursor()
-    cursor.execute("DROP TABLE IF EXISTS STOPS")
-
-
-def createTable():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="123456",
-        database="ligne95"
-    )
-    cursor = mydb.cursor()
-    cursor.execute(
-        "CREATE TABLE STOPS (id INT AUTO_INCREMENT primary key NOT NULL, id_stop INT, name VARCHAR(100) NOT NULL, latitude FLOAT,longitude FLOAT, destination VARCHAR(100),\
->>>>>>> d3c1ef6d02ba6a408743e7d07109ba862b1bbf87
         type VARCHAR(100), arrival VARCHAR(10), departure VARCHAR(10));"
     )
 
@@ -81,7 +58,6 @@ def getDataFromDatabase():
     print(myresult)
     data = []
 
-<<<<<<< HEAD
 def getTime(start):
 
   mydb = mysql.connector.connect(
@@ -99,10 +75,3 @@ def getTime(start):
 
   print(myresult)
   return myresult
-=======
-    for row in myresult:
-        data.append({"id": row[0], "id_stop": row[1], "name": row[2], "latitude": row[3], "longitude": row[4],
-                    "destination": row[5], "type": row[6], "arrival": row[7], "departure": row[8]})
-
-    return data
->>>>>>> d3c1ef6d02ba6a408743e7d07109ba862b1bbf87
