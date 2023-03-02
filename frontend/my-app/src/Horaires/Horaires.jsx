b7971dd1e8438b7eb32213f3d20ddf150f693486
Etienne Decourselle <decourselle.etienne@hotmail.fr>
2023-03-02 16:27:00 +0100

    Page Horaire

--- frontend/my-app/src/Horaires/Horaires.jsx
+++ frontend/my-app/src/Horaires/Horaires.jsx
@@ -10,18 +10,36 @@ const Horaires = () => {
 
     const [horaires, setHoraires] = useState([]);
     const [isSearched, setSearched] = useState(false);
-    const [isOptionSelected, setOptionSelected] = useState(false);
+    const [isDirectionSelected, setDirectionSelected] = useState(false);
+    const [stops, setStops] = useState([]);
 
-    const handleSubmit = async (e) => {
-        console.log('test')
-        e.preventDefault()
+    const handleSubmit = async () => {
         const listeHoraires = await fetch(`http://localhost:8000/stops`);
         const horaires = await listeHoraires.json()
-        console.log(horaires)
         setHoraires(horaires);
-        console.log(horaires)
-        setSearched(true);
+        findAllStops(horaires)
     };
+
+    const showStops = () => {
+        setDirectionSelected(true)
+    }
+
+    const showDirection = () => {
+
+    }
+
+    const showResult = () => {
+        setSearched(true);
+    }
+
+    const findAllStops = (arr) => {
+        let stopsArray = [];
+        for (let index = 0; index < arr.length; index++) {
+            const stop = arr[index]
+            stopsArray.push({ value: stop.id, label: stop.name })
+        }
+        setStops(stopsArray)
+    }
 
     const options = [
         { value: [{ type: 'tram' }, { ligne: 'A' }], label: 'Tram A' },
@@ -37,27 +55,25 @@ const Horaires = () => {
         { value: [{ type: 'Bus' }, { ligne: '95' }], label: 'Bus 95' },
     ]
 
+
+
     return (
         <>
             <Header />
             <Link className='retour' to='/'>
                 <img className='fleche' src={fleche_gauche} alt='retour' />
             </Link>
-            <div>
-                <Select className='select' options={options} />
-                {/* {isOptionSelected ?
-                <div id='results' className='horaires-results'>
-                    {horaires.map(horaire => {
-                        return (
-                            <p className='horaire'>Arrivée : {horaire.arrival} / Départ : {horaire.departure}</p>
-                        )
-                    })}
-                </div>
-                :
-                <>
-                </>
-            }  */}
-                <button className='submit-button' onClick={(e) => handleSubmit(e)}>Rechercher</button>
+            <div className='form'>
+                <Select className='select' options={options} onChange={() => { handleSubmit(); showStops() }} />
+                {isDirectionSelected ?
+                    <div id='results' className='horaires-results'>
+                        <Select className='select' options={stops} />
+                    </div>
+                    :
+                    <>
+                    </>
+                }
+                <button className='submit-button' type='submit' onClick={() => showResult()}>Rechercher</button>
             </div>
 
             {isSearched ?
@@ -65,7 +81,11 @@ const Horaires = () => {
                     {horaires.map(horaire => {
                         console.log("horaire de horaires.map : " + horaire)
                         return (
-                            <p className='horaire'>Arrivée : {horaire.arrival} / Départ : {horaire.departure}</p>
+                            <div className='listeStops'>
+                                <h3>{horaire.name}</h3>
+                                <p className='horaire'>Arrivée : {horaire.arrival}</p>
+                                <p className='horaire'>Départ : {horaire.departure}</p>
+                            </div>
                         )
                     })}
                 </div>
